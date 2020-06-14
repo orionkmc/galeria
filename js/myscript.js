408e5a9adb2617a291c7e782624ad347fee0032a
cesar karim <cesarim37@gmail.com>
2020-06-14 03:10:42 -0400

    zoom a imagenes

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+var inputs = document.querySelectorAll( '.demo-image' );
+Intense( inputs );
+
+/*for (i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('click', function() {
+    document.querySelector('main').style.display = 'none';
+    document.querySelector('body').classList.remove('impress-enabled');
+  });
+}
+
+*/
