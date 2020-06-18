c86239c53f8c4a3daadeb0b0ee872f6a52d568a8
cesar karim <cesarim37@gmail.com>
2020-06-18 17:46:28 -0400

    cambios paredes

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,9 @@
 var inputs = document.querySelectorAll( '.demo-image' );
 Intense( inputs );
 
-/*for (i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('click', function() {
-    document.querySelector('main').style.display = 'none';
-    document.querySelector('body').classList.remove('impress-enabled');
-  });
-}
+// for (i = 0; i < inputs.length; i++) {
+//   inputs[i].addEventListener('click', function() {
+//     document.querySelector('figure').style.width = 'auto';
+//   });
+// }
 
-*/
