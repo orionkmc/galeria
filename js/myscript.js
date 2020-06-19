2a27579c4fe1dd878556402167799a000de62fbf
cesar karim <cesarim37@gmail.com>
2020-06-18 21:52:29 -0400

    cambios en index

--- js/myscript.js
+++ js/myscript.js
@@ -7,3 +7,78 @@ Intense( inputs );
 //   });
 // }
 
+evaluar();
+window.addEventListener('hashchange', function() {
+  evaluar();
+});
+
+function evaluar(e){
+  a = location.href.split('#/')[1]
+  console.log(a);
+  if (a == 'entrada') {
+    allBlock();
+    document.querySelector('#pared16').style.display = "none";
+  } else if (a == 'pared2') {
+    allBlock();
+    document.querySelector('#pared5').style.display = "none";
+  } else if (a == 'pared4') {
+    allBlock();
+    document.querySelector('#pared7').style.display = "none";
+    document.querySelector('#pared10').style.display = "none";
+  } else if (a == 'pared5') {
+    allBlock();
+    document.querySelector('#pared2').style.display = "none";
+  } else if (a == 'pared6') {
+    allBlock();
+    document.querySelector('#pared20').style.display = "none";
+  } else if (a == 'pared7') {
+    allBlock();
+    document.querySelector('#pared4').style.display = "none";
+  } else if (a == 'pared8') {
+    allBlock();
+    document.querySelector('#pared19').style.display = "none";
+  } else if (a == 'pared9') {
+    allBlock();
+    document.querySelector('#pared18').style.display = "none";
+  } else if (a == 'pared10') {
+    allBlock();
+    document.querySelector('#pared13').style.display = "none";
+  } else if (a == 'pared11') {
+    allBlock();
+    document.querySelector('#pared17').style.display = "none";
+  } else if (a == 'pared12') {
+    allBlock();
+    document.querySelector('#pared15').style.display = "none";
+  } else if (a == 'pared13') {
+    allBlock();
+    document.querySelector('#pared10').style.display = "none";
+    document.querySelector('#pared7').style.display = "none";
+  } else if (a == 'pared15') {
+    allBlock();
+    document.querySelector('#pared12').style.display = "none";
+  } else if (a == 'pared16') {
+    allBlock();
+    document.querySelector('#entrada').style.display = "none";
+  } else if (a == 'pared17') {
+    allBlock();
+    document.querySelector('#pared11').style.display = "none";
+  } else if (a == 'pared18') {
+    allBlock();
+    document.querySelector('#pared9').style.display = "none";
+  } else if (a == 'pared19') {
+    allBlock();
+    document.querySelector('#pared8').style.display = "none";
+  } else if (a == 'pared20') {
+    allBlock();
+    document.querySelector('#pared6').style.display = "none";
+  } else {
+    allBlock();
+  }
+}
+
+function allBlock(){
+  inputs = document.querySelectorAll('.step');
+  for (i = 0; i < inputs.length; i++) {
+    inputs[i].style.display = "block";
+  }
+}
