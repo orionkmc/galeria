43b48ea2bee60fcf64aed525853e9ab49f2c6c85
kuai-mare carrasquero <orionkmc@gmail.com>
2021-05-10 11:55:25 -0400

    google analytics

--- index.php
+++ index.php
@@ -30,7 +30,17 @@
   <meta name="msapplication-TileColor" content="#ffffff"/>
   <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png"/>
   <meta name="theme-color" content="#ffffff"/>
+  <!-- Global site tag gtag.js - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179632238-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-179632238-1');
+  </script>
 </head>
+
 <body class="impress-not-supported">
   <div class="techo"></div>
   <div class="piso"></div>
